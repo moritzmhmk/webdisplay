1ec64512bb5f652630a759cc9b44eeb18c5421a3
moritzmhmk <moritzmhmk@gmail.com>
2018-03-20 20:09:02 +0100

    WIP: display works, touch is missing python-evdev

--- rootfs_scripts/post-image.sh
+++ rootfs_scripts/post-image.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+# HyperPixel LCD Settings
+dtoverlay=hyperpixel
+overscan_left=0
+overscan_right=0
+overscan_top=0
+overscan_bottom=0
+framebuffer_width=800
+framebuffer_height=480
+enable_dpi_lcd=1
+display_default_lcd=1
+dpi_group=2
+dpi_mode=87
+dpi_output_format=0x6f016
+display_rotate=2
+hdmi_timings=800 0 50 20 50 480 1 3 2 3 0 0 0 60 0 32000000 6
+
+# Use a basic GPIO backlight driver with on/off support
+dtoverlay=hyperpixel-gpio-backlight
+__EOF__
+
 # copy the files to the binaries_dir ("images")
 cp -r $BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH/rootfs_scripts/files/* ${BINARIES_DIR}
 
